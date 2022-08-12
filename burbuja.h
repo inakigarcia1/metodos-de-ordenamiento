b87d726de4d875e3ed6828bcfcc6000e09b0bd62
Bernanchet Martín <110435970+Zenturick@users.noreply.github.com>
2022-08-12 18:22:57 -0300

    Add files via upload

---
+++ burbuja.h
@@ -0,0 +1,40 @@
+void burbuja_mejorado(int vector[], int orden){
+	
+	int vectorMod[orden] = {0};
+	
+	for (int i = 0; i < orden; i++){
+		vectorMod[i] = vector[i];
+	}
+	
+	int bandera=1, temporal, ciclos;
+	
+	printf ("\n*** Usted ha escogido el metodo Burbuja Mejorado ***\n\n");
+
+	printf ("\n***El arreglo a ordenar es:***\n\n");
+	for(int i = 0; i < orden; i++)
+	{
+		printf(" [%d] ", vectorMod[i]);
+	}
+	
+	printf("\n\n");
+	
+	for(int i = 0; i < (orden - 1) && bandera == 1; i++)
+	{
+		bandera = 0;
+		for (int j = 0; j < (orden - 1); j++)
+		{
+			if(vectorMod[j] > vectorMod[j+1])
+			{
+				bandera = 1;
+				temporal = vectorMod[j];
+				vectorMod[j] = vectorMod[j+1];
+				vectorMod[j+1] = temporal;
+			}
+		}
+		for(int i = 0; i < orden; i++)
+		{
+			printf(" [%d] ", vectorMod[i]);
+		}
+		printf("\n");
+	}
+}
